6f03f6388b44832b61d5f1fd5147dfd1256c5be3
Ujjwal Tiwari <ujjwaltiwari153@gmail.com>
2024-11-08 22:03:37 +0530

    numbers

---
+++ src/com/ujjwal/NumbersExample.java
@@ -0,0 +1,16 @@
+package com.ujjwal;
+
+public class NumbersExample {
+
+    public static void main(String[] args) {
+          print1 ( 1 );
+          print1 ( 2 );
+          print1 ( 3 );
+          print1 ( 4 );
+          print1 ( 5 );
+    }
+
+    static void print1(int n) {
+        System.out.println ( n );
+    }
+}
