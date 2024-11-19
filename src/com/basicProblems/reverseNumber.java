9c6ba5f90b4ac0cd6e6ef3ba7db4bcc9f05e0bb6
Ujjwal Tiwari <ujjwaltiwari153@gmail.com>
2024-11-19 09:32:30 +0530

    sum of n

--- src/com/basicProblems/reverseNumber.java
+++ src/com/basicProblems/reverseNumber.java
@@ -1,7 +1,20 @@
 package com.basicProblems;
 
 public class reverseNumber {
-    public static void main(String[] args) {
 
+    static int sum =0;
+    static void revMethod1(int n){
+        if(n==0){
+            return;
+        }
+        int rem =n%10;
+        sum =sum*10 +rem;
+        revMethod1 (n/10);
     }
+
+    public static void main(String[] args) {
+       revMethod1 ( 1342 );
+        System.out.println (sum);
+    }
+
 }
