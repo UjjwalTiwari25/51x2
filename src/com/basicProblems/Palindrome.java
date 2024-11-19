5a5aa97b936fe701b9fa4005392453be18887b5d
Ujjwal Tiwari <ujjwaltiwari153@gmail.com>
2024-11-19 10:47:38 +0530

    sum of n

--- src/com/basicProblems/Palindrome.java
+++ src/com/basicProblems/Palindrome.java
@@ -3,12 +3,14 @@ package com.basicProblems;
 public class Palindrome {
     public static void main(String[] args) {
         int number = 1234321; // Example number
-        if (Palindrome(number)) {
+        if (isPalindrome(number)) {
             System.out.println(number + " is a Palindrome.");
         } else {
             System.out.println(number + " is NOT a Palindrome.");
         }
     }
+
+
     static int rev(int n){
         //now we need base condition
         //sometimes we might need some additional variable in the argument ...in that case ,make another function as helper function
@@ -25,5 +27,22 @@ public class Palindrome {
     }
     static boolean Palindrome(int n) {
         return n == rev ( n );
+    }
+
+    //By converting into String and comparing first digit and last digit
+    static boolean isPalindrome(int n) {
+        String numStr = Integer.toString(n); // Convert number to string
+        int start = 0;  // Pointer to the first digit
+        int end = numStr.length() - 1; // Pointer to the last digit
+
+        while (start < end) {
+            if (numStr.charAt(start) != numStr.charAt(end)) {
+                return false; // If any mismatch, it's not a palindrome
+            }
+            start++; // Move to the next digit from the start
+            end--;   // Move to the previous digit from the end
+        }
+
+        return true; // All digits matched, it's a palindrome
     }
 }
