a00a15345e6ea0a3e740bc8a8b7e66ce8f890dc5
Ujjwal Tiwari <ujjwaltiwari153@gmail.com>
2024-11-19 22:39:09 +0530

    Linear Search

--- src/com/recursion/problem/Array/LinearSearchWithRecursion.java
+++ src/com/recursion/problem/Array/LinearSearchWithRecursion.java
@@ -1,4 +1,18 @@
 package com.recursion.problem.Array;
 
 public class LinearSearchWithRecursion {
+    public static void main(String[] args) {
+      int[]arr ={1,4,8,6,0};
+        System.out.println (findIndex ( arr,6,0 ));
+    }
+
+    static int findIndex(int[] arr,int target , int index){
+        if(index==arr.length){
+            return -1;
+        }
+        if(arr[index]==target){
+            return index;
+        }
+        return findIndex ( arr,target,index+1 );
+    }
 }
