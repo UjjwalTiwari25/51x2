15a864a531ae87d266eb0f388d4a9dd252f9db0e
Ujjwal Tiwari <ujjwaltiwari153@gmail.com>
2024-11-19 20:34:39 +0530

    sum of n

--- src/com/recursion/problem/Array/isArraySorted.java
+++ src/com/recursion/problem/Array/isArraySorted.java
@@ -1,4 +1,16 @@
 package com.recursion.problem.Array;
 
 public class isArraySorted {
+    public static void main(String[] args) {
+        int[]arr ={1,2,3,5,6,8,9};
+        System.out.println (isSorted ( arr,0 ));
+
+    }
+    static boolean isSorted(int[] arr, int index){
+        //base condition
+        if(index==arr.length-1){
+            return true;
+        }
+        return arr[index]< arr[index+1] && isSorted ( arr ,index+1 );
+    }
 }
