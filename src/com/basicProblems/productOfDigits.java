3822cef974003fc095eef07df28b5fdd8f136d79
Ujjwal Tiwari <ujjwaltiwari153@gmail.com>
2024-11-22 11:30:44 +0530

    New Setup

--- src/com/basicProblems/productOfDigits.java
+++ src/com/basicProblems/productOfDigits.java
@@ -5,6 +5,13 @@ public class productOfDigits {
         int result =product ( 505);
         System.out.println (result);
     }
+    /**
+     * This function takes a number and returns the product of all its digits.
+     * It does this by recursively calling itself with the number divided by 10
+     * and multiplying the result by the remainder of that division.
+     * @param n the number to calculate the product of its digits
+     * @return the product of all the digits in n
+     */
     static int product(int n){
         if(n/10<1){
             return n;
