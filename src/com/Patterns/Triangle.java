07a1c9c9cdfa787d9d331cfcfba044b8dbee73a4
Ujjwal Tiwari <ujjwaltiwari153@gmail.com>
2024-11-26 19:06:04 +0530

    search

---
+++ src/com/Patterns/Triangle.java
@@ -0,0 +1,20 @@
+package com.Patterns;
+
+public class Triangle {
+    public static void main(String[] args) {
+        triangle(5,0);
+
+    }
+    static void triangle(int row , int col){
+        if(row==0) {
+            return;
+        }
+        if(col<row){
+            System.out.print(" * ");
+            triangle(row,col+1);
+        }else{
+            System.out.println();
+            triangle(row-1,0);
+        }
+    }
+}
