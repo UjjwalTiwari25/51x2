a9a255928a513807f05b292f3475cb94c2d6e7c0
Ujjwal Tiwari <ujjwaltiwari153@gmail.com>
2024-11-26 09:47:44 +0530

    recursion

--- src/com/basicProblems/ReturnAll_IndexOfTarget.java
+++ src/com/basicProblems/ReturnAll_IndexOfTarget.java
@@ -1,4 +1,20 @@
 package com.basicProblems;
 
+import java.util.ArrayList;
+
 public class ReturnAll_IndexOfTarget {
+    public static void main(String[] args) {
+        int[]arr={2,3,1,4,4,5};
+        System.out.println(findAllIndex(arr,4,0,new ArrayList<>()));
+
+    }
+    static ArrayList<Integer> findAllIndex(int[] arr,int target,int index , ArrayList<Integer> list){
+        if(index==arr.length){
+            return list;
+        }
+        if(arr[index]==target){
+            list.add ( index );
+        }
+        return findAllIndex ( arr,target,index+1,list );
+    }
 }
