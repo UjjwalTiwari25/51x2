19ef4c53fcd0eafd66f9d8fdf1ba4c19704c8161
Ujjwal Tiwari <ujjwaltiwari153@gmail.com>
2024-11-26 10:45:05 +0530

    recursion

--- src/com/basicProblems/ReturnAll_IndexOfTarget.java
+++ src/com/basicProblems/ReturnAll_IndexOfTarget.java
@@ -5,9 +5,13 @@ import java.util.ArrayList;
 public class ReturnAll_IndexOfTarget {
     public static void main(String[] args) {
         int[]arr={2,3,1,4,4,5};
-        System.out.println(findAllIndex(arr,4,0,new ArrayList<>()));
+//        ArrayList<Integer> ans=findAllIndex( arr,4,0,new ArrayList<> () );
+//        System.out.println(ans);
+        System.out.println(findAllIndex2(arr,4,0));
 
     }
+    //Method 1: passing arraylist to parameter
+
     static ArrayList<Integer> findAllIndex(int[] arr,int target,int index , ArrayList<Integer> list){
         if(index==arr.length){
             return list;
@@ -16,5 +20,27 @@ public class ReturnAll_IndexOfTarget {
             list.add ( index );
         }
         return findAllIndex ( arr,target,index+1,list );
+    }
+
+
+
+
+    //Method 2 : passing answer indivisually to each function call
+
+    static ArrayList<Integer> findAllIndex2(int[] arr,int target,int index ){
+
+        ArrayList<Integer> list=new ArrayList<> ();
+        if(index==arr.length){
+            return list;
+        }
+        //this will contain answer for that call only
+        if(arr[index]==target){
+            list.add ( index );
+        }
+        ArrayList<Integer> ansfromBellowCalls=findAllIndex2 ( arr,target,index+1);
+
+        list.addAll ( ansfromBellowCalls );
+
+        return list;
     }
 }
