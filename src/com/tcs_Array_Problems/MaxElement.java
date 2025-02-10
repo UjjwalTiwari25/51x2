924b7976382a24d2fc788eefc111ee4d4616f7b8
Ujjwal Tiwari <ujjwaltiwari153@gmail.com>
2025-02-10 18:34:52 +0530

    max

--- src/com/tcs_Array_Problems/MaxElement.java
+++ src/com/tcs_Array_Problems/MaxElement.java
@@ -2,8 +2,9 @@ package com.tcs_Array_Problems;
 
 public class MaxElement {
     public static void main(String[] args) {
-        int[] arr={5,4,7,3,8,9};
-        System.out.println(max(arr));
+        int[] arr={5,4,3,3,8,9};
+//        System.out.println(max(arr));
+        System.out.println(maxInRange(arr,1,2));
     }
     static int max(int arr[]){
         int max= arr[0];
@@ -15,5 +16,16 @@ public class MaxElement {
 
         }
        return max;
+    }
+    static int maxInRange(int arr[],int start,int end){
+        int maxInRange = arr[start];
+        for (int i = start; i < end; i++) {
+            if(arr[i]> maxInRange){
+                maxInRange =arr[i];
+
+            }
+
+        }
+        return maxInRange;
     }
 }
