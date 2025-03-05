84db1cbf328ed5e0a133a128947f4aa3046f2de9
Ujjwal Tiwari <112298492+UjjwalTiwari25@users.noreply.github.com>
2025-03-05 23:07:32 +0530

    Update sumOfDigits.java

--- src/com/basicProblems/sumOfDigits.java
+++ src/com/basicProblems/sumOfDigits.java
@@ -1,14 +1,14 @@
-package com.basicProblems;
+// package com.basicProblems;
 
-public class sumOfDigits {
-    public static void main(String[] args) {
-      int ans =sum( 987654321 );
-        System.out.println (ans);
-    }
-    static int sum(int n) {
-        if (n/10<1){
-            return n;
-        }
-        return sum(n/10) +n%10;
-    }
-}
+// public class sumOfDigits {
+//     public static void main(String[] args) {
+//       int ans =sum( 987654321 );
+//         System.out.println (ans);
+//     }
+//     static int sum(int n) {
+//         if (n/10<1){
+//             return n;
+//         }
+//         return sum(n/10) +n%10;
+//     }
+// }
