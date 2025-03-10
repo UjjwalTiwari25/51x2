539a75338ad92831de9f4f6a9880d2a6918506e4
UjjwalTiwari25 <ujjwaltiwari153@gmail.com>
2025-03-10 21:52:22 +0530

    first commit

--- src/com/tcs/NumberSystem/find_indexOfRightMostSetBit.java
+++ src/com/tcs/NumberSystem/find_indexOfRightMostSetBit.java
@@ -1,10 +1,1 @@
-package com.tcs.NumberSystem;
 
-public class find_indexOfRightMostSetBit {
-    public static void main(String[] args) {
-        int num=42;
-        int i =0;
-        System.out.println();
-
-
-}
